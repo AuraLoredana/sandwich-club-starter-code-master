f01bbff1f0e7e24f2f27fb76bb559dfff97dfb08
AuraLoredana <popescuauraloredana@gmail.com>
2018-03-04 17:52:24 +0200

    DetailActivity modified

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,6 +3,7 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +14,14 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 
 import org.w3c.dom.Text;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
+
+    int position;
+    String[] sandwiches;
+    String json;
+    Sandwich sandwich;
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
@@ -24,28 +32,29 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
-        TextView originIv = findViewById((R.id.origin_tv));
-        TextView description = findViewById((R.id.description_tv));
-        TextView ingredients = findViewById((R.id.ingredients_tv));
-        TextView alsoknownTv = findViewById((R.id.also_known_tv));
 
         Intent intent = getIntent();
         if (intent == null) {
+            Log.e("null", "Intent null");
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
+            Log.e("not found", "position default");
             closeOnError();
             return;
         }
 
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwiches = getResources().getStringArray(R.array.sandwich_details);
+        json = sandwiches[position];
+        sandwich = null;
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
+            Log.e("no data", "sandwich null");
+
             closeOnError();
             return;
         }
@@ -56,10 +65,6 @@ public class DetailActivity extends AppCompatActivity {
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
-        ingredients.setText(sandwich.getIngredients().toString());
-        originIv.setText(sandwich.getPlaceOfOrigin());
-        description.setText(sandwich.getDescription());
-        alsoknownTv.setText(sandwich.getAlsoKnownAs().toString());
     }
 
     private void closeOnError() {
@@ -68,6 +73,32 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        TextView mAlsoKnownAs = (TextView) findViewById(R.id.also_known_tv);
+        TextView mIngredients = (TextView) findViewById(R.id.ingredients_tv);
+        TextView mPlaceOfOrigin = (TextView) findViewById(R.id.origin_tv);
+        TextView mDescription = (TextView) findViewById(R.id.description_tv);
+        if (sandwich.getPlaceOfOrigin().isEmpty()) {
+            mPlaceOfOrigin.setText("No Data");
+        } else {
+            mPlaceOfOrigin.setText(sandwich.getPlaceOfOrigin());
+        }
+        if (sandwich.getAlsoKnownAs().isEmpty()) {
+            mAlsoKnownAs.setText("No Data");
+        } else {
+            mAlsoKnownAs.setText(listModel(sandwich.getAlsoKnownAs()));
+        }
 
+
+        mDescription.setText(sandwich.getDescription());
+        mIngredients.setText(listModel(sandwich.getIngredients()));
+
+    }
+
+    public StringBuilder listModel(List<String> list) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            stringBuilder.append(list.get(i)).append("\n");
+        }
+        return stringBuilder;
     }
 }
